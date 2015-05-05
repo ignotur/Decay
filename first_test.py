8732b1dd1f10abb33fca27e5f9f468bc8250d7f1
ignotur <ignotur@gmail.com>
2015-05-05 19:52:28 +0200

    The Ohmic decay occur at correct timescale. There are some problem with the Hall evolution.

--- first_test.py
+++ first_test.py
@@ -22,7 +22,10 @@ def crust_model_grid (n, m):
 	for j in range (0, n):
 		for k in range (0, m):
 			ne[j][k], sigma[j][k] = crust_model (r_grid[j], mu_grid[k])
-			
+	
+	
+#	print sigma
+
 	return [r_grid, mu_grid, ne, sigma]
 
 def initial_pure_poloidal (n, m, B):
@@ -41,7 +44,7 @@ def initial_pure_poloidal (n, m, B):
 
 	for j in range (2, n):
 		for k in range (0, m):
-		 	phi  [j][k] = 1e20 * sqrt(1 - np.power(mu_grid[k], 2.0)) / r_grid[j]  ## This generates another component
+		 	phi  [j][k] = 5e29 * sqrt(1 - np.power(mu_grid[k], 2.0)) /r_grid[j]  ## This generates another component
 			I [j][k] = 0
 #			I  [j][k] = 1e16 * cos(3.0*acos(mu_grid[k])) / pow(r_grid[j], 1.0)
 #			phi  [j][k] = 1e20 * sin(3.0*acos(mu_grid[k])) / pow(r_grid[j], 1.0)
@@ -75,7 +78,7 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 	c = 3.0e10
 	c2 = pow(c, 2.0)
 	r_star = 1.0e6 
-	h = 1e8 ## Should be analysed again (!!!)
+	h =1.0e7 ## Should be analysed again (!!!)
 	delta_r  = 0.1 * r_star / float(n)
 	delta_mu = 2.0 / float(m)
 	delta_t  = 0.25 * h * delta_r * delta_mu 
@@ -88,7 +91,7 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 
 #        print delta_t / 3.2e7 
 
-	for j in range (1, n-2):                 ## r  change 
+	for j in range (1, n-1):                 ## r  change 
 		for k in range (1, m-1):         ## mu change 
 			## for each point inside the grid we compute time derivative 
 			## for scalar functions phi and I
@@ -114,8 +117,12 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 			
 			if (j==1):
 				phi_rrr    = (phi[j+2][k] - 2.0 * phi[j+1][k] + phi[j-1][k] + phi[1][k]) / (2.0 * pow(delta_r, 3.0))
+			elif (j==n-2):
+				phi_rrr    = (-phi[j][k] - 2.0 * phi[j+1][k] + phi[j-1][k] - phi[j-2][k]) / (2.0 * pow(delta_r, 3.0))
 			else:
 				phi_rrr    = (phi[j+2][k] - 2.0 * phi[j+1][k] + phi[j-1][k] - phi[j-2][k]) / (2.0 * pow(delta_r, 3.0))
+
+			
 
 #			if (k==m-1):
 #				print 'Here m-1: ', k, m-1
@@ -189,7 +196,9 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 #			print '----------------------'
 #			print - const1 / ne[j][k] * (phi_r * (-I_mu)/r2 + phi_mu*I_r/r2)
 #			print c2 / (4.0 * 3.1415926 * sigma[j][k]) * grad_shafranov
-	
+		
+#			print delta_omega_delta_phi_delta_Phi
+
 			delta_I_delta_t   =  - r2 * sintheta2 * delta_omega_delta_phi_delta_Phi - I[j][k] * delta_hi_delta_phi_delta_I + c*c / (4.0 * 3.1415926 * sigma[j][k]) * ( grad_shafranov_I - delta_I_delta_sigma / sigma[j][k] ) 
 
 #			print '---------------------------------++++'
@@ -203,7 +212,40 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 #			print delta_I_delta_t
 
 			phi_new[j][k] = phi[j][k] + delta_t * delta_phi_delta_t
-			I_new[j][k]   = I[j][k] + delta_t * delta_I_delta_t
+			I_new[j][k]   = I[j][k]   + delta_t * delta_I_delta_t
+
+
+
+	l_polinom = 50
+	al = []
+	for l in range (1,l_polinom): 
+		var = 1.0 / (l + 1.0) * sqrt(3.1415926*(2.0*l+1.0)) 
+		res = 0.0
+		for j in range (0, m-1):
+			legendre_fun = lpmn(1, l_polinom, mu_grid[j])[0][1]
+			res = res + 0.5 * (phi[n-3][j] + phi[n-3][j+1]) * legendre_fun[l] * delta_mu #/sqrt(1.0 - pow(mu_grid[j],2.0))
+		var = res * var
+		al.append(var)
+	
+
+	for k in range (1, m-1):
+
+#		legendre_fun = lpmn(1, 20, z)[0][1]
+	
+		res = 0.0
+		legendre_fun = lpmn(1, l_polinom, mu_grid[k])[0][1]
+		for l in range (1,l_polinom):
+			res = res - al[l-1] / pow(r_star, 1.0) * sqrt((2.0*l+1.0) / (4.0*3.1415926))  * legendre_fun[l]  * sqrt(1.0 - pow(mu_grid[k], 2.0))
+		res = res * 2 * delta_r
+
+
+		phi_new[n-1][k] = phi[n-3][k] + res
+#		phi_new[n-2][k] = phi[n-4][k] + res 
+
+		I_new[n-1][k] = 0.0
+		I_new[n-2][k] = 0.0
+		I_new[0][k] = 0.0
+		phi_new[0][k] = 0.0
 
 	for j in range (0, n):
 		phi_new[j][0] = 0.0
@@ -211,40 +253,14 @@ def one_time_step (n, m, phi, I, r_grid, mu_grid):
 		I_new[j][0] = 0.0
 		I_new[j][m-1] = 0.0
 
-	for k in range (1, m-1):
 
-#		legendre_fun = lpmn(1, 20, z)[0][1]
-		l_polinom = 16
-		al = []
-		for l in range (1,l_polinom): 
-			var = pow(r_star, l) / (l + 1.0) * sqrt(3.1415926*(2.0*l+1.0)) 
-			res = 0.0
-			for j in range (1, m-1):
-				legendre_fun = lpmn(1, l_polinom, 0.5*(mu_grid[j] + mu_grid[j+1]))[0][1]
-				res = res + 0.5 * (phi[n-3][j] + phi[n-3][j+1]) * legendre_fun[l] * delta_mu/sqrt(1.0 - pow(mu_grid[j],2.0))
-			var = res * var
-			al.append(var)
-		
-		res = 0.0
-		legendre_fun = lpmn(1, l_polinom, mu_grid[k])[0][1]
-		for l in range (1,l_polinom):
-			res = res - al[l-1] / pow(r_star, l+1) * sqrt((2.0*l+1.0) / (4.0*3.1415926)) * sqrt(1.0 - pow(mu_grid[k], 2.0)) * legendre_fun[l]
-		res = res * 2 * delta_r
+#	phi_new[n-1][1] = 0.0
+#	phi_new[n-1][m-2] = 0.0
 
-
-		phi_new[n-1][k] = phi[n-3][k] + res
-		phi_new[n-2][k] = phi[n-4][k] + res 
-
-		I_new[n-1][k] = 0.0
-		I_new[n-2][k] = 0.0
-		I_new[0][k] = 0.0
-		phi_new[0][k] = 0.0
-
-
-	return [phi_new, I_new, delta_t]
+	return [phi_new, I_new, delta_t, al]
 
 n = 40
-m = 100
+m = 40
 
 
 r_grid, mu_grid, phi, I = initial_pure_poloidal (n, m, 10)
@@ -253,19 +269,55 @@ plot_res (n, m, phi, I, r_grid, mu_grid)
 #plot_res (n, m, I, r_grid, mu_grid)
 
 delta_t = 0
+r_star = 1.0e6
+step=[]
+evol=[]
+al0=[]
+al1=[]
+al2=[]
+al3=[]
+al4=[]
+al5=[]
 
-for k in range (0, 7000):
+for k in range (0, 1000):
 	print k, np.max(phi), np.min(phi), np.max(I), np.min(I), delta_t/3.2e7*k
-	phi, I, delta_t = one_time_step (n, m, phi, I, r_grid, mu_grid)
+	phi, I, delta_t, al = one_time_step (n, m, phi, I, r_grid, mu_grid)
+	al0.append(fabs(al[0])/pow(r_star, 2.0))
+	al1.append(fabs(al[1])/pow(r_star, 3.0))
+	al2.append(fabs(al[2])/pow(r_star, 4.0))
+	al3.append(fabs(al[3])/pow(r_star, 5.0))
+	al4.append(fabs(al[4])/pow(r_star, 6.0))
+	al5.append(fabs(al[5])/pow(r_star, 7.0))
+	step.append(delta_t/3.2e7*k)
+	evol.append(np.max(phi))
 	if (k % 100 == 10):
 		plot_res (n, m, phi,I, r_grid, mu_grid)
 #		plot_res (n, m, I, r_grid, mu_grid)
 
+ax = plt.subplot(1,1,1)
+plt.plot(step, al0, label='1')
+plt.plot(step, al1, label='2')
+plt.plot(step, al2, label='3')
+plt.plot(step, al3, label='4')
+plt.plot(step, al4, label='5')
+plt.plot(step, al5, label='6')
+plt.xscale('log')
+plt.yscale('log')
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels)
+plt.show()
 
-print r_grid
-print mu_grid
-print phi
-print I
+#print '----------------------------'
+#print al0
+#print al1
+#print al2
+#print al3
+
+
+#print r_grid
+#print mu_grid
+#print phi
+#print I
 
 
 #print 'Look here -- ', len(r_grid)
